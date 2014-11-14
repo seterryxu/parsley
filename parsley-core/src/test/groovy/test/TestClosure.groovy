fc77b0a085c37d38c4878af7268a96a52a5918d7
seterryxy <cs.terryxu@gmail.com>
2014-11-14 14:23:29 +0800

    +6

--- parsley-core/src/test/groovy/test/TestClosure.groovy
+++ parsley-core/src/test/groovy/test/TestClosure.groovy
@@ -3,38 +3,36 @@ package test
 import static org.junit.Assert.*
 
 import org.junit.Test
-import org.seterryxu.navigator.framework.core.uom.PRequestImpl;
-import org.seterryxu.navigator.framework.core.uom.PResponseImpl;
 
 class TestClosure {
 
 	@Test
 	public void test() {
-//		def aa=[2, 1, 3]
-//
-//		aa.each {
-//			println it
-//			if(it==1)
-//				return
-//		}
-//
-//		for(def a:aa){
-//			println a
-//			if(a==1)
-//				break
-//		}
+		//		def aa=[2, 1, 3]
+		//
+		//		aa.each {
+		//			println it
+		//			if(it==1)
+		//				return
+		//		}
+		//
+		//		for(def a:aa){
+		//			println a
+		//			if(a==1)
+		//				break
+		//		}
 
 		TestBean.metaClass.invokeMethod={String name,Object argz->
-				if(name.startsWith('do'))
-					println "$name, $argz"
-			}
-		
+			if(name.startsWith('do'))
+				println "$name, $argz"
+		}
+
 		new TestBean().doAbc()
-		
-		PRequestImpl pres;
+
+		PRequestImpl pres
 	}
-	
+
 	def $(){
-		
+
 	}
 }
